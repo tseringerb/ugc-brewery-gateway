1b03bb2fd0955d5e00132c3ca9d85b381f9ed6d5
taskurim <borystserinher@gmail.com>
2021-11-25 17:28:00 +0300

    Initial commit

---
+++ src/main/java/com/ugc/brewery/ugcbrewerygateway/UgcBreweryGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ugc.brewery.ugcbrewerygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UgcBreweryGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UgcBreweryGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ugc/brewery/ugcbrewerygateway/UgcBreweryGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ugc.brewery.ugcbrewerygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UgcBreweryGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
